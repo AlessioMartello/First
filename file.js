c39c87ebb93b9bdd9b3dc5fbdcfb32bc60a37c34
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2018-12-23 21:34:47 +0100

    unfinished slideshow function added

--- file.js
+++ file.js
@@ -1,28 +1,26 @@
-"use strict";
+"use strict"
 
-changeImg;
+function showDivs(n) {
+  var i;
+  var image = document.getElementsByClassName("slideShow");
+  if (n > image.length ) {slideIndex=1;}
+  // if reached end of array, starts over
+  if (n < 1) {slideIndex = image.length;}
+//if slideIndex is less than 0 then show the last image instead
+  for (i = 0; i < image.length; i++)          //
+    {image[i].style.display = "none";}        //execute this
 
-var i = 0; //start index
-var images = document.getElementsByClassName ("images_full"); // images to be looped through
-var time = 3000; //time is ms to switch between images
-
-//image list - array values with image paths
-images[0] = "2018-12-09/1028.jpg";
-images[1] = "2018-12-09/965.jpg";
-images[2] = "2018-12-09/1089.jpg";
-
-//Function to change the images
-
-function changeImg(){
-  document.slide.src = images[i];
-
-  if ( i < images.length - 1)
-  {i++;
-  } else {
-    1 = 0;
-  }
-
-  setTimeout("changeImg()", time);
+  image[slideIndex-1].style.display = "block";
 }
 
-window.onload = changeImg;
+var slideIndex = 1; //set the picture to number one
+showDivs(slideIndex); // call the function showDivs for the current slideIndex (number) to dislay it
+
+function plusDivs (n) {
+  showDivs(slideIndex += n); //code to be executed
+  }
+
+// This is what concerns the buttons
+// plusDivs adds or subtracts to the slideIndex hence
+// changing the image.
+// so it does function showDivs for varying slide Indices
