600dd6f8cc07be0977ceb1bd3493ba5c874bdccb
Cole O'Brien <gastrocole@gmail.com>
2019-01-03 22:48:21 +0000

    lesson on pulling before pushing and merging conflicts

---
+++ Art.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <?php $title = '[i am here to mess with you]'; ?>
+  <?php $metaTags = 'tag1 tag2'; ?>
+  <?php $currentPage = '[teach you to pull before you commit]'; ?>
+  <?php include('components/head.php'); ?>
+
+  <body id="page-top" class="background-class-here">
+    <?php include('components/header.php'); ?>
+
+    <!-- Include content here -->
+    i am not going to work
+    
+    <?php include('components/footer.php'); ?>
+    <?php include('components/include-scripts.php'); ?>
+  </body>
+
+</html>
