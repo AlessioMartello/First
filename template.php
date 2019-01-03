23e76b4e561a0c4d31e879930df5e95f665b4b94
Cole O'Brien <gastrocole@gmail.com>
2019-01-03 21:50:38 +0000

    added php template for pages

---
+++ template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  
+  <?php $title = 'Corinthian - Makers and Restorers of Fine Furniture'; ?>
+  <?php $metaTags = 'tag1 tag2'; ?>
+  <?php $currentPage = 'index'; ?>
+  <?php include('head.php'); ?>
+
+  <body id="page-top">
+    <?php include('navigation.php'); ?>
+    <div id="contents" class="not-blurred">
+      <?php include('header.php'); ?>
+      <!-- Include content below -->
+      <?php include('footer.php'); ?>
+    </div>
+    <?php include('include-scripts.php'); ?>
+  </body>
+
+</html>
