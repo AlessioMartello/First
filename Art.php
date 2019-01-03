f6fa3d21b53b93a83532c1866fe8c723ae189024
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2019-01-03 23:52:17 +0100

    added Art.php

---
+++ Art.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <?php $title = 'Art'; ?>
+  <?php $metaTags = 'tag1 tag2'; ?>
+  <?php $currentPage = 'Art'; ?>
+  <?php include('components/head.php'); ?>
+
+  <body id="page-top" class="backgroundFormatting">
+    <?php include('components/header.php'); ?>
+
+    <!-- Include content here -->
+    <?php include('content/Art.php'); ?>
+
+
+    <?php include('components/footer.php'); ?>
+    <?php include('components/include-scripts.php'); ?>
+  </body>
+
+</html>
