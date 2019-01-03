f2c9a357e87988602b80709f8dba65adcd93e81e
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2019-01-04 00:13:38 +0100

    added Carousel component and placed on art.php

--- content/Art.php
+++ content/Art.php
@@ -1,4 +1,4 @@
-<? php include("component/drop-down") ?>
+<? php include("components/drop-down") ?>
 
 <div class="introTextContainer">
 <div class= "introText">
@@ -6,14 +6,4 @@
 </div>
 </div>
 
-  <img src="img/1140.jpg" class= "artSlideShow" alt="Image of ...">
-  <img src="img/1150.jpg" class= "artSlideShow" alt="Image of ...">
-  <img src="img/011.jpg" class= "artSlideShow" alt="Image of ...">
-
-<div class = artSlideShowButtons>
-  <button id="artLeftButton" align= "left" type="button" onclick="plusDivs(-1)">&#10094;</button>
-  <button id="artRightButton" align= "right" type="button" onclick="plusDivs(1)">&#10095;</button>
-</div>
-
-<script src="js/picture-slideshow.js">
-</script>
+<? php include("components/Carousel") ?>
