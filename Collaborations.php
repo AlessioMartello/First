6637f2721a83aa8ed1d8df1e9e232ec214e8c2a1
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2019-01-04 22:15:33 +0100

    Fixed margin bug on index page

---
+++ Collaborations.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <?php $title = 'Collaborations'; ?>
+  <?php $metaTags = 'tag1 tag2'; ?>
+  <?php $currentPage = 'Collaborations'; ?>
+  <?php include('components/head.php'); ?>
+
+  <body id="page-top" class="backgroundFormatting">
+    <?php include('components/header.php'); ?>
+
+    <!-- Include content here -->
+    <?php include('content/Collaborations.php'); ?>
+
+
+    <?php include('components/footer.php'); ?>
+    <?php include('components/include-scripts.php'); ?>
+  </body>
+
+</html>
---
+++ content/Collaborations.php
@@ -0,0 +1,5 @@
+<?php include("components/drop-down.php") ?>
+
+<p> Watch this space.
+
+Add Durack's Info work etc </p>
