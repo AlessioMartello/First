b79b85c6502ae23775014e4bcfd4b0375c988d2e
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2019-01-08 14:59:29 +0100

    fixed carousel

--- components/head.php
+++ components/head.php
@@ -8,7 +8,6 @@
   <!-- Custon global css for each page -->
   <link rel="stylesheet" href="css/styles.css">
 
-
   <!-- Shared css -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
@@ -18,5 +17,8 @@
   <!-- Custon css for specific page -->
   <link rel="stylesheet" href="css\<?php echo($currentPage); ?>.css" >
 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
--- content/Collaborations.php
+++ content/Collaborations.php
@@ -1,11 +1,14 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <?php include("components/Drop-down/Collaborations.php") ?>
 
 <div class="container">
       <h1>COLLABORATIONS</h1>
         <h2>Daniel Durack</h2>
 <div class="btn-group m-5">
-  <button type="button" class="btn btn-lg btn-info">phuckit.creative</button>
-  <button type="button" class="btn btn-lg btn-info">About</button>
+  <a type="button" class="btn btn-lg btn-info" href="http://picdeer.com/phuckit.creative">phuckit.creative</a>
+  <a type="button" class="btn btn-lg btn-info">About</a>
 </div>
 
 </div>
