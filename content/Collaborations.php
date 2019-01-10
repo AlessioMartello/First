f79d350eeb80d5b0f6d064d774709465a4d0c462
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2019-01-10 15:50:37 +0100

    spaced footer icons, spaced collaboration wording

--- content/Collaborations.php
+++ content/Collaborations.php
@@ -1,10 +1,11 @@
 <?php include("components/Drop-down/Collaborations.php") ?>
 
 <div class="container" id= "txtDurack">
-
-    </div>
       <h1>COLLABORATIONS</h1>
         <h2>Daniel Durack</h2>
+</div>
+    </div>
+
 <div class="btn-group m-5">
   <a type="button" class="btn btn-lg btn-info" href="http://picdeer.com/phuckit.creative">phuckit.creative</a>
   <a type="button" class="btn btn-lg btn-info">About</a>
