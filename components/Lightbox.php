625cea069bec009c3de0657c1a43ec21e83b7d3f
AlessioMartello123 <alessio.martello@hotmail.co.uk>
2019-01-11 16:06:05 +0100

    changed pictures to bootstrap gallery and media icons greyed

---
+++ components/Lightbox.php
@@ -0,0 +1,112 @@
+<section class= "container" id="lightbox">
+  <div class="row" id= "full">
+    <figure class="col-sm-4">
+      <img src="img/1028.jpg" alt="#">
+    </figure>
+
+    <figure class="col-sm-4">
+      <img src="img/965.jpg" alt="#">
+    </figure>
+
+    <figure class="col-sm-4">
+      <img src="img/1089.jpg" alt="#">
+    </figure>
+
+  </div>
+
+  <div class="row">
+    <figure class="col-sm-4" id="full">
+      <img src="img/040.jpg" alt="#">
+    </figure>
+
+    <figure class="col-sm-4">
+      <img src="img/004.jpg" alt="#">
+    </figure>
+
+    </div>
+
+    <div class="row" id= "square">
+      <figure class="col-sm-4">
+        <img src="img/404.jpg" alt="#">
+      </figure>
+
+      <figure class="col-sm-4">
+        <img src="img/1156.jpg" alt="#">
+      </figure>
+
+      <figure class="col-sm-4">
+        <img src="img/105.jpg" alt="#">
+      </figure>
+
+    </div>
+
+    <div class="row" id = "square">
+      <figure class="col-sm-4">
+        <img src="img/101.jpg" alt="#">
+      </figure>
+
+      <figure class="col-sm-4">
+        <img src="img/092.jpg" alt="#">
+      </figure>
+
+      <figure class="col-sm-4">
+        <img src="img/045.jpg" alt="#">
+      </figure>
+    </div>
+    <div class = "row" id="square">
+      <figure class="col-sm-4">
+        <img src="img/021.jpg" alt="#">
+      </figure>
+
+      <figure class="col-sm-4">
+        <img src="img/086.jpg" alt="#">
+      </figure>
+
+      <figure class="col-sm-4">
+        <img src="img/1119.jpg" alt="#">
+      </figure>
+      </div>
+
+      <div class = "row" id="square">
+        <figure class="col-sm-4">
+          <img src="img/1111.jpg" alt="#">
+        </figure>
+
+        <figure class="col-sm-4">
+          <img src="img/1104.jpg" alt="#">
+        </figure>
+
+        <figure class="col-sm-4">
+          <img src="img/4945.jpg" alt="#">
+        </figure>
+        </div>
+
+        <div class="row" id = "horizontal">
+          <figure class="col-sm-6">
+            <img src="img/1153.jpg" alt="#">
+          </figure>
+
+          <figure class="col-sm-6">
+            <img src="img/990.jpg" alt="#">
+          </figure>
+
+          <figure class="col-sm-6">
+            <img src="img/966.jpg" alt="#">
+          </figure>
+        </div>
+
+
+        <div class="row" id = "pano">
+          <figure class="img-fluid">
+            <img src="img/1025.jpg" alt="#">
+          </figure>
+
+          <figure class="img-fluid">
+            <img src="img/102.jpg" alt="#">
+          </figure>
+
+          <!-- <figure class="img-fluid">
+            <img src="#" alt="#">
+          </figure> -->
+        </div>
+</section>
--- content/Collaborations.php
+++ content/Collaborations.php
@@ -1,6 +1,6 @@
 <?php include("components/Drop-down/Collaborations.php") ?>
 
-<div class="container" id= "txtDurack">
+<div class="container" id= "txt">
       <h1>COLLABORATIONS</h1>
       <hr />
         <h2>Daniel Durack</h2>
@@ -10,5 +10,10 @@
 
 <div class="btn-group m-5">
   <a type="button" class="btn btn-lg btn-info" href="http://picdeer.com/phuckit.creative">phuckit.creative</a>
-  <a type="button" class="btn btn-lg btn-info">About</a>
+  <a type="button" class="btn btn-lg btn-info" href="#">About</a>
+</div>
+
+<div class="container" id= "txt">
+      <hr />
+        <h2>Joseph Mceachron</h2>
 </div>
